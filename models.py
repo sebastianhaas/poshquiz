ee7b91aa6111418bd5d7bb8e6e22cb12d84211aa
sebastianhaas <sebastianhaas@users.noreply.github.com>
2014-08-08 22:43:03 +0200

    Initial commit

---
+++ models.py
@@ -0,0 +1,27 @@
+from peewee import Model, DateTimeField, CharField, BooleanField, TextField, PostgresqlDatabase
+
+DATABASE = 'poshquiz'
+HOST = 'localhost'
+PORT = '5432'
+USER = 'poshquiz'
+PASSWORD = 'poshquiz'
+
+database = PostgresqlDatabase(host=HOST, port=PORT, database=DATABASE, user=USER, password=PASSWORD)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Message(BaseModel):
+    datetime = DateTimeField()
+    text = TextField()
+    outgoing = BooleanField()
+    messenger = CharField(max_length=50)
+
+
+# simple utility function to create tables
+def create_tables():
+    database.connect()
+    Message.create_table()
